5079bb326e553119cdc6cf2198d39b9e05a2cbee
Luis Lima <llima@easygosa.net>
2022-12-13 22:03:42 -0600

    Tipos de datos

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,35 @@
-//Impresión de tu primer hola mundo
-console.log("Hola Mundo");
+//declaración de una variable de tipo strign
+var texto = "Hola Mundo desde JavaScript";
+
+//declaración de una variable de tipo num 
+var numero = 1;
+
+//declaración de una variable de tipo object
+var objeto = {
+    animal : "perro",
+    nombre : "shark",
+    patas : 4
+}
+
+//declaración de una variable de tipo boolean
+var boolean = false;
+console.log(boolean);
+
+//declaración de una variable de tipo function
+function MyFristFunction (){
+    console.log(MyFristFunction);
+}
+
+//declaración de una variable de tipo Symbol
+var simbolo = Symbol();
+
+//declaración de una variable de tip class
+class persona {
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+//declaración de una variable se tipo array
+var carros = ['Toyota','BMW','Audi'];
