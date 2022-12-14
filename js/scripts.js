350bb2621f828ed3eac68898732e3a25ac04a2fd
Luis Lima <llima@easygosa.net>
2022-12-13 22:30:01 -0600

    Concatenación de datos

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,27 @@
-//declaración de una variable de tipo strign
-var texto = "Hola Mundo desde JavaScript";
+//concatenación de variables de tipo string
+var texto1 = "Hello";
+var texto2 = "World";
+var ambosTextos =  texto1 + texto2;
+//El texto de la linea 4 se mostrara sin espacios
+var ambosTextos2 = texto1 + " " + texto2;
+console.log(ambosTextos2);
+//con la concatenacón de del espacio de verá correctamente
 
-//declaración de una variable de tipo num 
-var numero = 1;
+//ejemplo con  numeros
+var num1 = 2;
+var num2 = 6;
+var ambosNum = num1 + num2;
+console.log(ambosNum);
+//en la linea 6 el resultado de la impresión será 8
 
-//declaración de una variable de tipo object
-var objeto = {
-    animal : "perro",
-    nombre : "shark",
-    patas : 4
-}
+var ambosNumYambosTextos = num1 + texto1 + num2 + num1;
+console.log(ambosNumYambosTextos);ambosNumYambosTextos2
+//la impresión de la linea 18 tomara todos los valores como string
 
-//declaración de una variable de tipo boolean
-var boolean = false;
-console.log(boolean);
+var ambosNumYambosTextos2 = texto1 + (num1 + num2) + texto2;
+console.log(ambosNumYambosTextos2);
+//utilizando parentesis en la concatenación de la linea 21 tomara como valores numericos las variables de ese tipo
 
-//declaración de una variable de tipo function
-function MyFristFunction (){
-    console.log(MyFristFunction);
-}
-
-//declaración de una variable de tipo Symbol
-var simbolo = Symbol();
-
-//declaración de una variable de tip class
-class persona {
-    constructor(nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
-    }
-}
-
-//declaración de una variable se tipo array
-var carros = ['Toyota','BMW','Audi'];
+var ambosNumYambosTextos3 = texto1 + " " + (num1 + num2) + " " + texto2;
+console.log(ambosNumYambosTextos3);
+//de esta manera estaría todo concatenado de la manera correcta
